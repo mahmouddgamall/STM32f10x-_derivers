0781c210474270b575adbefdbc89d0af03a42813
mahmouddgamall <mahmouddgamall93@gmail.com>
2020-04-20 00:57:11 +0200

    Signed-off-by: mahmouddgamall <mahmouddgamall93@gmail.com>UART & DMA

---
+++ UART/2-Handler/HUART_cnfg.h
@@ -0,0 +1,12 @@
+/**********************************************************************************************
+Author :Mohamed Ibrahem, Mahmoud Gamal
+Version:1.0
+Date:5 April 2020
+Description:This Header file is used to interface with the HUART module in STM32f10x evaluation kit
+***********************************************************************************************/
+#ifndef HUART_CNFG_H
+#define HUART_CNFG_H
+
+
+
+#endif
